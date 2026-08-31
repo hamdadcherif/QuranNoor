d3e54d0b70be6ebf02fa27bdc0f7ad0103519915
hamdad mohammed cherif <hmdadmohammedcherif@gmail.com>
2026-08-31 16:12:25 +0100

    Fix duplicate BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import SurahPage from './pages/SurahPage';
@@ -7,12 +7,10 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/QuranNoor">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/surah/:id" element={<SurahPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/surah/:id" element={<SurahPage />} />
+    </Routes>
   );
 }
 
